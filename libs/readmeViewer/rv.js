e5d2bf908703e7315210725809e09ae1e0502bbf
Theo Armour <t.armour@gmail.com>
2014-03-30 15:40:25 -0700

    r2

---
+++ libs/readmeViewer/rv.js
@@ -0,0 +1,58 @@
+// (c) Theo Armour ~ 2014-03-30 ~ r1 ~ MIT License 
+
+	var converter;
+	var content;
+
+	function init() {
+		if ( !converter ) {
+			converter = new Showdown.converter();
+// Styles for the doc
+			var css = document.body.appendChild( document.createElement('style') );
+			css.innerHTML = 'body { font: normal 12pt sans-serif; margin: 0; overflow: hidden; }';
+
+	//Styles for menu and content
+			var basics = 'border: 3px double #eee; overflow-x: hidden; overflow-y: auto; padding: 10px; position: absolute; ';
+			var horizontalsMenu = 'left: 10%; width: 15%; ';
+			var horizontalsContent = 'left: 30%; width: 60%; ';
+			var verticals = 'height: ' + ( window.innerHeight * 0.88 ) + 'px; top: 60px; ';
+
+// Content panel
+			content = document.body.appendChild( document.createElement( 'div' ) );
+			content.style.cssText = basics + horizontalsContent + verticals ;
+
+			window.addEventListener('hashchange', init, false );
+		}
+
+		if ( !location.hash ) {
+			displayPage( '#readme.md#rm' );
+		} else {
+			displayPage( location.hash );
+		}
+	}
+
+	function displayPage( hash ) {
+		var hashes = hash.split('#');
+
+// Fetch and show the content file
+		content.innerHTML = converter.makeHtml( requestFile( hashes[1] ) );
+
+// Update window title to match H1 of content file
+		document.title = content.innerHTML.match( /<h1(.*?)>(.*?)<\/h1>/ )[2];
+
+
+// Update URL hash
+		if ( hashes[1] === 'readme.md' ) {
+// if at home page, delete any hash and clean up the history
+			history.pushState( '', document.title, window.location.pathname );
+		} else {
+			location.hash = hash;
+		}
+	}
+
+// Fetch a file
+	function requestFile( fname ) {
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.open( 'GET', fname, false );
+		xmlHttp.send( null );
+		return xmlHttp.responseText;
+	}
--- readme.md
+++ readme.md
@@ -3,23 +3,12 @@ mobile-data.github.io
 
 A place for cool things
 
-[Songs]( http://mobile-data.github.io/songs/index.html )  
-[Places Demo]( http://mobile-data.github.io/places-demo/index.html )  
+[Songs]( http://mobi-data.github.io/songs/index.html )  
+[Places Demo]( http://mobi-data.github.io/places-demo/index.html )  
 
 
+<br>
 
+<br>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-<small>[source code]( https://github.com/mobile-data/ )</small>
+<small>source code: [github.com/mobi-data]( https://github.com/mobi-data/ )</small>
